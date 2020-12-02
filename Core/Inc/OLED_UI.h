a93147ce9034c9d02fd6713a895c92118a642e9e
Liuke <37175026+Maucke@users.noreply.github.com>
2020-12-02 16:40:25 +0800

    增加模式预定义

--- Core/Inc/OLED_UI.h
+++ Core/Inc/OLED_UI.h
@@ -14,14 +14,30 @@ extern "C" {
   class OLED_UI : public virtual OLED_GFX {
     
     public: 
-      OLED_UI(void);
+      			OLED_UI(void);
 			OLED_STATUS MtRect(int x,int y,int w,int h,int step,uint16_t color);
 			OLED_STATUS SUITitleShow(int step,uint16_t color);
 			OLED_STATUS SUICornShow(void);
+	  
 			void SUI_In();
 			void SUI_Out();
-			OLED_STATUS SUIMainShow();
 			void SUIDataPrss();
+			OLED_STATUS SUIMainShow();
+	  
+			void NUI_In();
+			void NUI_Out();
+			void NUIDataPrss();
+			OLED_STATUS NUIMainShow();
+
+			void TUI_In();
+			void TUI_Out();
+			void TUIDataPrss();
+			OLED_STATUS TUIMainShow();
+
+			void MUI_In();
+			void MUI_Out();
+			void MUIDataPrss();
+			OLED_STATUS MUIMainShow();
 		
     private:  
   };
